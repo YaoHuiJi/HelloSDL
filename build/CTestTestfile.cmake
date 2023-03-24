6663257602fd769c68ddcd4f14a09c144dee2ac7
YaoHuiJi <yaohj@hotmail.com>
2023-03-24 09:38:19 +0800

    remove build

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/yaohuiji/LearnSpace/HelloSDL
-# Build directory: /Users/yaohuiji/LearnSpace/HelloSDL/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
