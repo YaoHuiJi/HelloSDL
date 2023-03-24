336fd9ed75b170e46e4aef2102fb91249e067775
YaoHuiJi <yaohj@hotmail.com>
2023-03-24 11:43:28 +0800

    add images

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,45 @@
-#include <stdio.h>
 #include <SDL.h>
+#include <stdio.h>
 
-int main()
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(int argc, char* args[])
 {
-    printf("hello sdl!");
+    SDL_Window* window = NULL;
+
+    SDL_Surface* screenSurface = NULL;
+
+    if( SDL_Init( SDL_INIT_VIDEO ) ){
+        printf("SDL cound not initialize! SDL_Error:%s\n", SDL_GetError());
+    }
+    else{
+        window = SDL_CreateWindow( "Hello SDL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        
+        if(window == NULL){
+            printf("WIdnows could not be created! SDL_Error:%s\n", SDL_GetError());
+        }
+        else{
+            screenSurface = SDL_GetWindowSurface(window);
+
+            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xCC, 0xCC, 0xCC ) );
+            
+            SDL_UpdateWindowSurface(window);
+
+            SDL_Event e;  bool quit = false; 
+
+            while( quit == false ){ 
+                while( SDL_PollEvent( &e ) ){ 
+                    if( e.type == SDL_QUIT ) quit = true; 
+                } 
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+
     return 0;
 }
