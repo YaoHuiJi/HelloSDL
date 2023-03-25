9c679764da9963781be2f1aee51ce9875599698f
YaoHuiJi <yaohj@hotmail.com>
2023-03-25 16:49:18 +0800

    lazyfoo lession 08 done & 绘制了贝塞尔曲线

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,8 @@ SDL_Texture* loadTexture(string path);
 
 SDL_Texture* gTexture = NULL;
 
+void drawARectangle(int x=SCREEN_WIDTH/4, int y=SCREEN_HEIGHT/4, int w=SCREEN_WIDTH/2, int h=SCREEN_HEIGHT/2);
+
 bool init(){
     bool success = true;
 
@@ -46,7 +48,7 @@ bool init(){
             }
             else
             {
-                SDL_SetRenderDrawColor( gRenderer, 0x33, 0x33, 0x33, 0xFF );
+                SDL_SetRenderDrawColor( gRenderer, 0x00, 0x00, 0x00, 0xFF );
 
                 int imgFlags = IMG_INIT_JPG;
 
@@ -115,6 +117,7 @@ bool loadLogo(int8_t& logoIndex){
 				stretchRect.w = w/scale;
 				stretchRect.h = SCREEN_HEIGHT;
 
+        SDL_SetRenderDrawColor( gRenderer, 0x00, 0x00, 0x00, 0xFF );
         SDL_RenderClear(gRenderer);
         SDL_RenderCopy(gRenderer, gTexture, NULL, &stretchRect);
         SDL_RenderPresent(gRenderer);
@@ -136,6 +139,73 @@ void close(){
     SDL_Quit();
 }
 
+void drawARectangle(int x, int y, int w, int h){
+    SDL_SetRenderDrawColor( gRenderer, 0x00, 0x00, 0x00, 0xFF );
+    SDL_RenderClear(gRenderer);
+
+    SDL_SetRenderDrawColor(gRenderer,0xF1,0xA0,0x50,0xFF);
+    SDL_Rect rect = {x, y, w, h};
+    SDL_RenderFillRect(gRenderer,&rect);
+
+    SDL_Rect outlineRect = { SCREEN_WIDTH / 6, SCREEN_HEIGHT / 6, SCREEN_WIDTH * 2 / 3, SCREEN_HEIGHT * 2 / 3 };
+    SDL_SetRenderDrawColor( gRenderer, 0x00, 0xFF, 0x00, 0xFF );        
+    SDL_RenderDrawRect( gRenderer, &outlineRect );
+
+    SDL_SetRenderDrawColor( gRenderer, 0x00, 0x00, 0xFF, 0xFF );
+    SDL_RenderDrawLine( gRenderer, 0, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT / 2 );
+
+    SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0x00, 0xFF );
+    
+    for( int i = 0; i < SCREEN_HEIGHT; i += 4 )
+    {
+        SDL_RenderDrawPoint( gRenderer, SCREEN_WIDTH / 2, i );
+    }
+
+    SDL_RenderPresent(gRenderer);
+}
+
+void drawBezierCurve(SDL_Renderer* renderer, SDL_Point p0, SDL_Point p1, SDL_Point p2, SDL_Point p3, float step)
+{
+    SDL_SetRenderDrawColor( gRenderer, 0x00, 0x00, 0x00, 0xFF );
+    SDL_RenderClear(gRenderer);
+
+    SDL_SetRenderDrawColor(renderer, 0xFF, 00, 0, 255);
+    SDL_RenderDrawPoint(renderer, p0.x, p0.y);
+
+    SDL_SetRenderDrawColor(renderer, 0xFF, 0x20, 0, 255);
+    SDL_RenderDrawPoint(renderer, p1.x, p1.y);
+
+    SDL_SetRenderDrawColor(renderer, 0xFF, 0x30, 0, 255);
+    SDL_RenderDrawPoint(renderer, p2.x, p2.y);
+
+    SDL_SetRenderDrawColor(renderer, 0xFF, 0x40, 0, 255);
+    SDL_RenderDrawPoint(renderer, p3.x, p3.y);
+
+    float t = 0.0f;
+    while (t <= 1.0f)
+    {
+        // 计算贝塞尔曲线上的点
+        float u = 1.0f - t;
+        float tt = t * t;
+        float uu = u * u;
+        float uuu = uu * u;
+        float ttt = tt * t;
+        SDL_Point p = {
+            static_cast<int>(uuu * p0.x + 3 * uu * t * p1.x + 3 * u * tt * p2.x + ttt * p3.x),
+            static_cast<int>(uuu * p0.y + 3 * uu * t * p1.y + 3 * u * tt * p2.y + ttt * p3.y)
+        };
+
+        // 绘制贝塞尔曲线上的点
+        SDL_SetRenderDrawColor(renderer, 123, 125, 0, 255);
+        SDL_RenderDrawPoint(renderer, p.x, p.y);
+
+        // 增加步长
+        t += step;
+    }
+
+     SDL_RenderPresent(gRenderer);
+}
+
 void handleEvent(int8_t& imageIndex){
     SDL_Event e;
     bool quit = false; 
@@ -148,17 +218,32 @@ void handleEvent(int8_t& imageIndex){
                 // printf("按下按键: %d\n", e.key.keysym.sym);
                 switch (e.key.keysym.sym)
                 {
-                case SDLK_ESCAPE:
-                    quit = true;
-                    break;
-                case SDLK_EQUALS:
-                    loadLogo(++imageIndex);
-                    break;
-                case SDLK_MINUS:
-                    loadLogo(--imageIndex);
-                    break;
-                default:
-                    break;
+                    case SDLK_ESCAPE:
+                        quit = true;
+                        break;
+                    case SDLK_EQUALS:
+                        loadLogo(++imageIndex);
+                        break;
+                    case SDLK_MINUS:
+                        loadLogo(--imageIndex);
+                        break;
+                    case SDLK_r:
+                        {
+                            drawARectangle();
+                        }
+                        break;
+                    case SDLK_b:
+                        {
+                            SDL_Point p0 = { 100, 100 };
+                            SDL_Point p1 = { 200, 50 };
+                            SDL_Point p2 = { 300, 200 };
+                            SDL_Point p3 = { 400, 100 };
+                            float step = 0.001f;
+                            drawBezierCurve(gRenderer, p0, p1, p2, p3, step);
+                        }
+                        break;
+                    default:
+                        break;
                 }
             }
         } 
